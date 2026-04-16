32856f7aac0d3aebcd9157e44f2782fcada7040e
Ngang Edwin N. <edwin@Mac.fritz.box>
2026-04-16 14:40:53 +0200

    Debug: show available env keys

--- functions/api/env-check.js
+++ functions/api/env-check.js
@@ -1,8 +1,9 @@
 export async function onRequestGet(context) {
   const hasResendKey = Boolean(context.env.RESEND_API_KEY);
+  const envKeys = Object.keys(context.env || {});
 
   return new Response(
-    JSON.stringify({ hasResendKey }),
+    JSON.stringify({ hasResendKey, envKeys }),
     {
       status: 200,
       headers: { "Content-Type": "application/json" },
